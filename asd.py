68148c787dea12ec038f22c4a7a7edc39403fb61
Brendan McGrath <brendan@redmandi.com>
2022-08-12 06:56:50 +1000

    asd: Add argparse and logging

--- asd.py
+++ asd.py
@@ -3,26 +3,39 @@
 from sys import argv
 from mpmath import mp, mpf, exp, log, lambertw, fmul, fdiv, fsub, fadd, plot
 from common import print_defaults, get_Is, get_nVt
+import logging
+import argparse
 
 def main(argv):
-  if len(argv) < 2 or len(argv) > 4:
-    print("Analyse Serial Diode")
-    print("Usage: asd <Vcc> <R> [<Is>] [<nVt>]")
-    print_defaults()
-  else:
-    Vcc = mpf(argv[0])
-    R = mpf(argv[1])
-    Is = mpf(get_Is(argv, 2))
-    nVt = mpf(get_nVt(argv, 2))
-    x = fdiv(fmul(fmul(Is,R),fsub(exp(fdiv(Vcc,nVt)),mpf(1))),nVt)
-    w = lambertw(x)
-    Id = fdiv(fmul(w,nVt),R)
-    Vd = fmul(log(fadd(fdiv(Id,Is),mpf(1))),nVt)
-    VR = fsub(Vcc, Vd)
-    IR = fdiv(VR, R)
-    print("VR: {}, IR: {}".format(VR, IR))
-    print("Vd: {}, Id: {}".format(Vd, Id))
-    plot([lambda x: fsub(Vcc,fmul(x,R)), lambda x: fmul(nVt,log(fadd(fdiv(x,Is),1)))], [0, fdiv(Vcc,R)], [0, Vcc])
+  parser = argparse.ArgumentParser(description='Analyse Serial Diode')
+  parser.add_argument('Vs', type=float, help='Voltage supply')
+  parser.add_argument('R', type=float, help='Resistor value in Ohms')
+  parser.add_argument('Is', type=float, nargs='?', default=1e-12, help='Diode Saturation current in Amps (default = 1e-12)')
+  parser.add_argument('N', type=float, nargs='?', default=1, help='Emission Coefficient (default = 1)')
+  parser.add_argument('--Vt', type=float, default=0.026, help='Thermal Voltage in Volts (default = 0.026)')
+  parser.add_argument('-g', '--graph', action='store_true', help='Draw a graph')
+  parser.add_argument('-v', '--verbose', action='store_true', help='Print debug')
+  args = parser.parse_args()
+
+  Vs = args.Vs
+  R = args.R
+  Is = args.Is
+  N = args.N
+  Vt = args.Vt
+  nVt = N*Vt
+  if args.verbose:
+    logging.basicConfig(format='%(levelname)s|%(message)s', level=logging.DEBUG)
+  logging.debug(f'Vs: {Vs}, R: {R}, Is: {Is}, N: {N}, Vt: {Vt}')
+  x = fdiv(fmul(fmul(Is,R),fsub(exp(fdiv(Vs,nVt)),mpf(1))),nVt)
+  w = lambertw(x)
+  Id = fdiv(fmul(w,nVt),R)
+  Vd = fmul(log(fadd(fdiv(Id,Is),mpf(1))),nVt)
+  VR = fsub(Vs, Vd)
+  IR = fdiv(VR, R)
+  print("VR: {}, IR: {}".format(VR, IR))
+  print("Vd: {}, Id: {}".format(Vd, Id))
+  if args.graph:
+    plot([lambda x: fsub(Vs,fmul(x,R)), lambda x: fmul(nVt,log(fadd(fdiv(x,Is),1)))], [0, fdiv(Vs,R)], [0, Vs])
 
 if __name__ == "__main__":
   main(argv[1:])
