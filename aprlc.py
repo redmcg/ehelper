e69e584b034300c63db0ace8368e0b9cef35c7ab
Brendan McGrath <brendan@redmandi.com>
2022-10-05 16:40:38 +1100

    Add scripts to analyse serial and parallel RLC circuits

---
+++ aprlc.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+from mpmath import sqrt, fmul, fdiv, pi, plot, power, fadd, log10
+import argparse
+import logging
+
+def main():
+  parser = argparse.ArgumentParser(description='Analyse a parallel RLC circuit')
+  parser.add_argument('R', type=float, help='Value of resistance in Ohms (Ωs)')
+  parser.add_argument('L', type=float, help='Value of inductance in Henries')
+  parser.add_argument('C', type=float, help='Value of capacitance in Farads')
+  parser.add_argument('-g', '--graph', action='store_true', help='Draw a graph')
+  parser.add_argument('-v', '--verbose', action='store_true', help='Print debug')
+  args = parser.parse_args()
+
+  r = args.R
+  l = args.L
+  c = args.C
+
+  if args.verbose:
+    logging.basicConfig(format='%(levelname)s|%(message)s', level=logging.INFO)
+  logging.info(f'R: {r}, L: {l}, C: {c}')
+
+  w0 = fdiv(1,sqrt(fmul(l,c)))
+  f0 = fdiv(w0,fmul(2,pi))
+  a = fdiv(1,fmul(2,fmul(r,c)))
+  d = fdiv(a,w0)
+  q = fmul(r,sqrt(fdiv(c,l)))
+  dw = fmul(2,a)
+  df = fdiv(dw,fmul(2,pi))
+
+  xl = lambda w: fmul(l,w)
+  xc = lambda w: fdiv(1,fmul(c,w))
+  z = lambda w: fdiv(1,fadd(fadd(fdiv(1,xl(w)),fdiv(1,xc(w))),fdiv(1,r)))
+  z0 = z(w0)
+
+  w0pow10 = lambda n: fmul(w0,power(10,n))
+  ratio2db = lambda r: fmul(20,log10(r))
+
+  print("ω₀: {}, f₀: {} Hz".format(w0, f0))
+  print("Δω: {}, Δf: {} Hz".format(dw, df))
+  print("Q: {}, ζ: {}".format(q, d))
+
+  logging.info(f'z0: {z0}')
+
+  if args.graph:
+    plot([lambda x: ratio2db(fdiv(z(w0pow10(x)),xl(w0pow10(x)))), lambda x: ratio2db(fdiv(z(w0pow10(x)),xc(w0pow10(x)))), lambda x: ratio2db(fdiv(z(w0pow10(x)),r)), lambda x: ratio2db(fdiv(z(w0pow10(x)),z0))], [-1,1], [ratio2db(fdiv(z(w0pow10(1)),xl(w0pow10(1)))),fadd(ratio2db(fdiv(z(w0pow10(-1)),xl(w0pow10(-1)))),1)])
+
+if __name__ == "__main__":
+  main()
---
+++ asrlc.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+from mpmath import sqrt, fmul, fdiv, pi, plot, power, fadd, log10
+import argparse
+import logging
+
+def main():
+  parser = argparse.ArgumentParser(description='Analyse a serial RLC circuit')
+  parser.add_argument('R', type=float, help='Value of resistance in Ohms (Ωs)')
+  parser.add_argument('L', type=float, help='Value of inductance in Henries')
+  parser.add_argument('C', type=float, help='Value of capacitance in Farads')
+  parser.add_argument('-g', '--graph', action='store_true', help='Draw a graph')
+  parser.add_argument('-v', '--verbose', action='store_true', help='Print debug')
+  args = parser.parse_args()
+
+  r = args.R
+  l = args.L
+  c = args.C
+
+  if args.verbose:
+    logging.basicConfig(format='%(levelname)s|%(message)s', level=logging.INFO)
+  logging.info(f'R: {r}, L: {l}, C: {c}')
+
+  xl = lambda w: fmul(l,w)
+  xc = lambda w: fdiv(1,fmul(c,w))
+  z = lambda w: fadd(fadd(xl(w),xc(w)),r)
+
+  w0 = fdiv(1,sqrt(fmul(l,c)))
+  f0 = fdiv(w0,fmul(2,pi))
+  x0 = xc(w0)
+  q = fdiv(x0,r)
+  dw = fdiv(w0,q)
+  a = fdiv(dw,2)
+  df = fdiv(dw,fmul(2,pi))
+  d = fdiv(a,w0)
+
+  z0 = z(w0)
+
+  w0pow10 = lambda n: fmul(w0,power(10,n))
+  ratio2db = lambda r: fmul(20,log10(r))
+
+  print("ω₀: {}, f₀: {} Hz".format(w0, f0))
+  print("Δω: {}, Δf: {} Hz".format(dw, df))
+  print("Q: {}, ζ: {}".format(q, d))
+
+  logging.info(f'z0: {z0}')
+
+  if args.graph:
+    plot([lambda x: ratio2db(fdiv(xl(w0pow10(x)),z(w0pow10(x)))), lambda x: ratio2db(fdiv(xc(w0pow10(x)),z(w0pow10(x)))), lambda x: ratio2db(fdiv(r,z(w0pow10(x)))), lambda x: ratio2db(fdiv(z0,z(w0pow10(x))))], [-1,1], [ratio2db(fdiv(xl(w0pow10(-1)),z(w0pow10(-1)))),fadd(ratio2db(fdiv(xl(w0pow10(1)),z(w0pow10(1)))),1)])
+
+if __name__ == "__main__":
+  main()
