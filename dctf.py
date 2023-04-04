9818875a51c853fc350fc63a87f08ca96065b1b3
Brendan McGrath <brendan@redmandi.com>
2023-04-04 20:39:38 +1000

    dctf.py: Add script to design a Cauer Topology Filter

---
+++ dctf.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python
+
+from mpmath import sin, cos, fadd, fsub, fmul, fdiv, power, sqrt, exp, log10, pi, mpc, cplot, fabs, sinh, asinh, cosh, log, coth, plot, polyval, acosh
+from os import remove
+import argparse
+import logging
+
+class Component:
+  def __init__(self, letter, number, value):
+    self.letter = letter
+    self.number = number
+    self.value = value
+
+  def print(self):
+    print("{}{}: {}".format(self.letter,self.number,self.value))
+    
+  def write_ngspice(self, f, innode, outnode):
+    f.write("{}{} {} {} {}\n".format(self.letter,self.number,innode,outnode,self.value.real))
+
+def write_ngspice(f, c, R, current):
+  if current:
+    source = "I"
+  else:
+    source = "V"
+
+  f.write("Cauer Topology Filter\n")
+  f.write("\n")
+  f.write("{}in in 0 DC 0 AC 1\n".format(source))
+
+  if len(c) == 3 and c[0].letter == "C":
+    nextinnode = "out"
+  else:
+    nextinnode = "in"
+    nextnode = 1
+
+  i = 1
+  for v in c:
+    innode = nextinnode
+    if v.letter == "C" or v.letter == "R" and i > 1:
+      outnode = 0
+    else:
+      if i < len(c) - 2:
+        outnode = nextnode
+        nextinnode = nextnode
+        nextnode = nextnode + 1
+      else:
+        outnode = nextinnode = "out"
+      
+    v.write_ngspice(f,innode,outnode)
+    i = i + 1
+
+  f.write("\n")
+  f.write(".AC DEC 100 0.01 1GIG\n")
+  f.write(".control\n")
+  f.write("run\n")
+  f.write("plot mag(out)/.5\n")
+  f.write(".endc\n")
+  f.write(".END\n")
+
+
+def main():
+  parser = argparse.ArgumentParser(description='Design Cauer Topology Filter')
+  parser.add_argument('C', type=float, nargs='+', help="List of coefficients from the filter's normalised transfer function (highest order first)")
+  parser.add_argument('-c', '--current', action='store_true', help='Input is a current source instead of voltage source')
+  parser.add_argument('-n', '--ngspice', metavar="filename", help='Write an ngspice file')
+  parser.add_argument('-f', '--force', action='store_true', help='Force creation of an ngfile by deleting an existing file')
+  parser.add_argument('-v', '--verbose', action='store_true', help='Print debug')
+  parser.add_argument('-g', '--graph', action='store_true', help='Display the complex output of the transfer function')
+  parser.add_argument('-w', '--wc', type=float, default=1, help='Set the cutoff value for the output components (in radians)')
+  parser.add_argument('-r', '--rs', type=float, default=1, help='Set the value of the source resistor')
+  args = parser.parse_args()
+
+  C = args.C
+
+  if args.verbose:
+    logging.basicConfig(format='%(levelname)s|%(message)s', level=logging.INFO)
+  logging.info(f'C: {C}, current: {args.current}, ngspice: {args.ngspice}, force: {args.force}')
+
+  c = []
+  R = args.rs
+  wc = args.wc
+  g = [R]
+
+  match len(C):
+    case 1:
+      parser.print_usage()
+      print("\nerror: at least two coefficients [C] must be specified")
+      exit(1)
+    case 2:
+      Rtot = 2.0
+      mod = fdiv(Rtot,C[1])
+      C1 = fmul(C[0],mod)
+      g.append(C1)
+      g.append(R)
+    case 3:
+      Rs = 1.0
+      C1 = fdiv(fmul(2,C[0]),C[1])
+      Rtot = fdiv(fmul(-C[2],fmul(power(C1,2),Rs)),fsub(C[0],fmul(C[2],power(C1,2))))
+      Rl = fsub(Rtot,Rs)
+      L2 = fmul(C1,Rl)
+      g.append(C1)
+      g.append(L2)
+      g.append(Rl)
+    case _:
+      parser.print_usage()
+      print("\nerror: more than three coefficients [C] is not currently supported")
+      exit(1)
+
+  i = 0
+  for v in g:
+    v = fdiv(v,wc)
+    if i == 0:
+      comp = "R"
+      v = R
+    elif i == len(g)-1:
+      Rl = g[len(g)-1]
+      comp = "R"
+      Rl = v = fmul(Rl,R)
+    elif i % 2 == 0 and not args.current or i % 2 == 1 and args.current:
+      comp = "L"
+      v = fmul(v,R)
+    else:
+      comp = "C"
+      v = fdiv(v,R)
+
+    v = Component(comp,i,v)
+    v.print()
+    c.append(v)
+    i = i + 1
+
+  if args.ngspice:
+    if args.force:
+      try:
+        remove(args.ngspice)
+      except:
+        print(f"\nWARNING: Couldn't force removal of {args.ngspice}")
+    try:
+      with open("/dev/stdout", "w") if args.ngspice == "-" else open(args.ngspice, "x") as f:
+        write_ngspice(f, c, R, args.current)
+    except FileExistsError:
+      print("\nCouldn't create {} as the file already exists. Use '-f' if you wish to replace it".format(args.ngspice))
+
+  def poly_string(poly, real_only = True):
+    poly_suffix = ["", "s", "s²", "s³"]
+
+    poly_s = ""
+    sep = ""
+    for i in range(len(poly)-1,-1,-1):
+      poly_s = poly_s + sep
+      sep = " + "
+      v = poly[len(poly)-1-i]
+      if real_only:
+        v = v.real
+      if v != 1.0 or i == 0:
+        poly_s = poly_s + f"{v}".replace("j","i")
+
+      poly_s = poly_s + (poly_suffix[i] if i < len(poly_suffix) else f"s^{i}")
+
+    return poly_s
+
+  Rtot = fadd(R,Rl)
+  component_poly = []
+  for i in range(0,len(C)):
+    k = len(C) - 1 - i
+    component_poly.append(fdiv(fmul(Rtot,C[i]),fmul(C[len(C)-1],power(wc,k))))
+
+  logging.info(f"Normalised: {poly_string(C)}")
+  logging.info(f"Component Poly: {poly_string(component_poly)}")
+
+  if args.graph:
+    cplot(lambda x: fabs(fdiv(Rl,polyval(component_poly,x))), re=[float(-2*wc), float(2*wc)], im=[float(-2*wc), float(2*wc)], points=100000, verbose=True)
+
+if __name__ == "__main__":
+  main()
